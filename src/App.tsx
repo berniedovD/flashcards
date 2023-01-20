5a9399c69b0f7c7c071ca742b7e13a3138a3e35e
rafisbytes <yoyorafo@gmail.com>
2023-01-20 14:19:46 -0500

    Added replay button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link, redirect } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import FlashcardList from "./FlashCardList";
 import Flashcardplay from "./FlashCardPlay";
+import GameOver from "./gameover";
 
 const App: React.FC = () => {
   const flashcards = [
@@ -38,7 +39,7 @@ const App: React.FC = () => {
             element={<FlashcardList flashcards={flashcards}></FlashcardList>}
           ></Route>
           <Route path="/play" element={<Flashcardplay flashcards={flashcards}/>}/>
-          <Route path="/gameover" element={<h1>You are done!!!</h1>}/>
+          <Route path="/gameover" element={<GameOver/>}/>
         </Routes>
       </div>
     </Router>
