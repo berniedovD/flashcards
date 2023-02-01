0d18a46f53877868d63c885b9f9e2ca9a0d48b8d
rafisbytes <yoyorafo@gmail.com>
2023-02-01 15:24:37 -0500

    Shows your score

--- src/flashcardPlay/gameover.tsx
+++ src/flashcardPlay/gameover.tsx
@@ -5,7 +5,11 @@ export default function GameOver(): JSX.Element {
     const navigate = useNavigate();
 
     let knownFlashcards = location.state.knownFlashcards;
-
+    
+    let FcCount = knownFlashcards.filter((FC: number) => FC !== 0).length;
+    let NFcount = knownFlashcards.filter((FC: number) => FC === 2).length;
+    let StLeCount = knownFlashcards.filter((FC: number) => FC === 1).length;
+    
     function playAgain(): void {
         navigate("/play", { replace: true });
     }
@@ -20,6 +24,7 @@ export default function GameOver(): JSX.Element {
        navigate("/play", { replace: true, state: {play: play}} );
     }
     return <div>
+       <h3>You know {NFcount} cards out of {FcCount}{StLeCount !== 0 ? ", but you are still learning " + StLeCount +" cards" : ""}.</h3>
        <button onClick={playAgain}>Play again</button>
        <button disabled={!knownFlashcards.includes(1)} onClick={playUnknown}>Play the cards you don't know</button>
     </div>
