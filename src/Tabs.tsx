6f485bc975d1b061668a7d507829c6338d3ea0dd
rafisbytes <yoyorafo@gmail.com>
2023-02-22 12:49:34 -0500

    Moved the tab code to a new file

---
+++ src/Tabs.tsx
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from "react";
+import Typography from "@mui/material/Typography";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import {useLocation, useNavigate, Outlet} from "react-router-dom";
+
+export default function TabNav() {
+  const LOCS = ["/", "/show", "/play", "/gameover"];
+  const LOCTAB = [0, 1, 2, 2];
+  function currentTab() {
+    return LOCTAB[LOCS.indexOf(location.pathname)];
+  }
+
+  const location = useLocation();
+  const [value, setValue] = useState<number | null>(currentTab());
+  useEffect(() => {setValue(currentTab())}, [location])
+  const navigate = useNavigate();
+
+  return (<Box>
+    <Typography align="center" variant="h2">Torah Flashcards</Typography>
+    <Tabs value={value} centered onChange={(event: React.SyntheticEvent, newValue: number) => setValue(newValue)}>
+       <Tab onClick={() => navigate("/")} label="Home"/>
+       <Tab onClick={() => navigate("/show")} label="Show Flashcards"/>
+       <Tab onClick={() => navigate("/play")} label="Play Flashcards"/>
+    </Tabs>
+    <Outlet/>
+  </Box>)
+}
