2b43fa1da39e15dba13adf5cf79a4191467c763d
rafisbytes <yoyorafo@gmail.com>
2023-03-02 11:04:37 -0500

    fixed bug

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -5,26 +5,32 @@ import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
 import {useLocation, useNavigate, Outlet} from "react-router-dom";
 
+type tabLink = { path: string, tabN: number };
+
 export default function TabNav() {
   const location = useLocation();
-  const LOCS = ["/", "/show", "/play", "/gameover"];
-  const LOCTAB = [0, 1, 2, 2];
+  const LOCS: tabLink[] = [
+    { path: "/", tabN: 0 },
+    { path: "/show", tabN: 1 },
+    { path: "/play", tabN: 2 },
+    { path: "/gameover", tabN: 2 }
+  ];
  
-  function currentTab() {
-    return LOCTAB[LOCS.indexOf(location.pathname)];
+  function currentTab(): number | undefined {
+    return LOCS.find((element: tabLink) => location.pathname.endsWith(element.path))?.tabN;
   }
  
-  const [value, setValue] = useState<number | null>(currentTab());
+  const [value, setValue] = useState<number | undefined>(currentTab());
   useEffect(() => {
    setValue(currentTab());
-  }, [location]); // eslint-disable-line react-hooks/exhaustive-deps
+  }, [location.pathname]); // eslint-disable-line react-hooks/exhaustive-deps
 
   const navigate = useNavigate();
 
   return (<Box>
     <Typography align="center" variant="h2">Torah Flashcards</Typography>
     <Tabs value={value} centered onChange={(event: React.SyntheticEvent, newValue: number) => setValue(newValue)} sx={{mb: 5}}>
-       <Tab onClick={() => navigate("")} label="Home"/>
+       <Tab onClick={() => navigate("./")} label="Home"/>
        <Tab onClick={() => navigate("show")} label="Show Flashcards"/>
        <Tab onClick={() => navigate("play")} label="Play Flashcards"/>
     </Tabs>
