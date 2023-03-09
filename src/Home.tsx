eb48fe9355d205eb638471c60e2ef1268a91c2fb
rafisbytes <yoyorafo@gmail.com>
2023-03-09 09:49:31 -0500

    Go to link

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,17 +1,22 @@
 import React from "react";
+
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent"
 import { CardActionArea } from '@mui/material';
 
+import { useNavigate } from "react-router-dom";
+
 // Hardcoded set of flashcard sets
 type FCSet = {name: string, id: number};
 const FCSets: FCSet[] = [{name: "Flashcard set 1", id: 0}, {name: "Flashcard set 2", id: 2}];
 
-function FCSetComp({card} : {card: string}) {
+
+function FCSetComp({card, id} : {card: string, id: number}) {
+  const navigate = useNavigate();
   return <Grid item xs={3}>
-    <Card>
+    <Card onClick={() => navigate("flashcard/" + id.toString())}>
       <CardActionArea>
         <CardContent>
           <Typography>{card}</Typography>
@@ -32,7 +37,7 @@ export default function Home() {
      alignItems="flex-start"     
      spacing={1}
    >
-     {FCSets.map((set: FCSet) => <FCSetComp card={set.name}/>)}
+     {FCSets.map((set: FCSet) => <FCSetComp card={set.name} id={set.id}/>)}
    </Grid>
   </Grid>
 }
